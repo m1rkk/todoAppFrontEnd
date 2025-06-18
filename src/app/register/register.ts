b0a10c283dce011371500577f49e5f8d50f1414b
--replace-all <mrakkus15@gmail.com>
2025-06-18 18:34:17 +0300

    feat: register page done

--- src/app/register/register.ts
+++ src/app/register/register.ts
@@ -4,31 +4,45 @@ import {UserModel} from '../models/userModel';
 import {Observable} from 'rxjs';
 import {TodoModel} from '../models/todoModel';
 import {UserService} from '../services/user-service';
+import {RouterLink, RouterOutlet} from '@angular/router';
+import {FormsModule} from '@angular/forms';
 
 
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    FormsModule
+  ],
   templateUrl: './register.html',
   standalone: true,
   styleUrl: './register.scss'
 })
-export class Register implements OnInit{
+export class Register{
   userService = inject(UserService)
   users = <Array<UserModel>>([])
-  user = {
-    username: 'mark',
-    email: 'mark@gmail.com',
-    password: 'io2jr3oiom1kmockw3',
-    todos: []
+
+  usernameValue = ''
+  emailValue =''
+  passwordValue =''
+  registerNewUser(){
+   let user = {
+      username: this.usernameValue,
+      email: this.emailValue,
+      password: this.passwordValue,
+      todos: []
+    }
+    this.userService.addNewUser(user)
+        .subscribe({next: (user)=>{
+          this.users.push(user);
+          alert('Success')
+          },error:(err) =>{
+          alert(err.message)
+          console.log(err)
+          }
+        })
   }
 
-  ngOnInit(): void {
-    this.userService.addNewUser(this.user)
-      .subscribe(user=>{
-        this.users.push(user)
-      })
-    console.log(this.users)
-  }
 }
